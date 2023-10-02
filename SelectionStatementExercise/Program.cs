1c15a21aa9e9c4ae430cc020302763bc70e58b61
James Yarbrough <jjarbrough@gmail.com>
2023-10-02 15:54:28 -0500

    added a loop so can leep goint until you guessed it

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -7,20 +7,25 @@ namespace SelectionStatementExercise
         static void Main(string[] args)
         {
             var r = new Random();
-            var favNumber = r.Next(1, 1000);
-            Console.WriteLine("guess a number");
-            var userInput = int.Parse(Console.ReadLine());
-            if(userInput < favNumber)
+            var favNumber = r.Next(1, 100);
+            bool guessedIt = false;
+            while (!guessedIt)
             {
-                Console.WriteLine("too low!");
-            }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine("too high!");
-            }
-            else
-            {
-                Console.WriteLine("Guessed it!");
+                Console.WriteLine("guess a number");
+                var userInput = int.Parse(Console.ReadLine());
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("too low!");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("too high!");
+                }
+                else
+                {
+                    Console.WriteLine("Guessed it!");
+                    guessedIt = true;
+                }
             }
         }
     }
